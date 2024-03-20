7b28f12ff4a9ff30d76f292a920ad02be5c0f47b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-03-20 11:38:52 +0100

    Update

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -3,11 +3,14 @@
 #include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <list>
 #include <numeric>
 #include <ranges>
 #include <string>
-#include <vector>
 #include <string_view>
+#include <vector>
+#include <functional>
+#include <format>
 
 using namespace std::literals;
 
@@ -186,7 +189,7 @@ TEST_CASE("lambda")
             return x * factor++;
         };
 
-		SECTION("default capture mode")
+        SECTION("default capture mode")
         {
             auto by_value = [&](int x) {
                 return x * factor;
@@ -210,14 +213,17 @@ TEST_CASE("lambda")
         std::cout << "AVG: " << calc_avg() << "\n";
     }
 
-	SECTION("mutable lambda")
-	{
-		int x = 10;
-		auto mutable_lambda = [x](int y) mutable { x += y; return x; };
+    SECTION("mutable lambda")
+    {
+        int x = 10;
+        auto mutable_lambda = [x](int y) mutable {
+            x += y;
+            return x;
+        };
 
-		CHECK(mutable_lambda(1) == 11);
-		CHECK(mutable_lambda(5) == 16);
-	}
+        CHECK(mutable_lambda(1) == 11);
+        CHECK(mutable_lambda(5) == 16);
+    }
 }
 
 TEST_CASE("sorting - STL & ranges")
@@ -239,7 +245,9 @@ TEST_CASE("sorting - STL & ranges")
 
 auto create_generator(int seed)
 {
-	return [seed]() mutable { return seed++; };
+    return [seed]() mutable {
+        return seed++;
+    };
 }
 
 void print(const auto& v, std::string_view name)
@@ -256,19 +264,287 @@ void print(const auto& v, std::string_view name)
 
 TEST_CASE("generator")
 {
-	auto gen_1 = create_generator(1);
-	CHECK(gen_1() == 1);
-	CHECK(gen_1() == 2);
-	CHECK(gen_1() == 3);
-	CHECK(gen_1() == 4);
+    auto gen_1 = create_generator(1);
+    CHECK(gen_1() == 1);
+    CHECK(gen_1() == 2);
+    CHECK(gen_1() == 3);
+    CHECK(gen_1() == 4);
 
-	auto gen_2 = create_generator(100);
-	CHECK(gen_2() == 100);
-	CHECK(gen_2() == 101);
-	CHECK(gen_2() == 102);
+    auto gen_2 = create_generator(100);
+    CHECK(gen_2() == 100);
+    CHECK(gen_2() == 101);
+    CHECK(gen_2() == 102);
 
-	std::vector<int> vec(100);
-	std::ranges::generate(vec, create_generator(665));
+    std::vector<int> vec(100);
+    std::ranges::generate(vec, create_generator(665));
 
-	print(vec, "vec");
+    print(vec, "vec");
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// callables
+
+bool is_even(uint64_t n)
+{
+    return n % 2 == 0;
+}
+
+struct IsEven
+{
+    bool operator()(uint64_t n) const
+    {
+        return n % 2 == 0;
+    }
+};
+
+auto lambda_is_even = [](uint64_t n) {
+    return n % 2 == 0;
+};
+
+namespace Explain
+{
+    namespace ver_1
+    {
+        template <typename InIter, typename Predicate>
+        InIter find_if(InIter first, InIter last, Predicate f)
+        {
+            for (InIter it = first; it != last; ++it)
+            {
+                if (f(*it))
+                {
+                    return it;
+                }
+            }
+
+            return last;
+        }
+    } // namespace ver_1
+
+    namespace ver_2
+    {
+        template <std::input_iterator Iter, typename Predicate>
+        Iter find_if(Iter first, Iter last, Predicate f)
+        {
+            for (Iter it = first; it != last; ++it)
+            {
+                if (f(*it))
+                {
+                    return it;
+                }
+            }
+
+            return last;
+        }
+    } // namespace ver_1
+
+	inline namespace ver_3
+	{
+		template <std::input_iterator Iter, typename F>
+        Iter find_if(Iter first, Iter last, F f)
+			requires std::predicate<F, decltype(*first)>
+        {
+            for (Iter it = first; it != last; ++it)
+            {
+                if (f(*it))
+                {
+                    return it;
+                }
+            }
+
+            return last;
+        }
+	}
+
+	namespace ver_4
+	{
+		template <std::input_iterator Iter, std::predicate<typename std::iterator_traits<Iter>::value_type> F>
+        Iter find_if(Iter first, Iter last, F f)
+        {
+            for (Iter it = first; it != last; ++it)
+            {
+                if (f(*it))
+                {
+                    return it;
+                }
+            }
+
+            return last;
+        }
+	}
+
+} // namespace Explain
+
+TEST_CASE("callable as func param")
+{
+    std::vector<int> vec = {1, 3, 5, 8, 9, 11};
+
+    std::random_access_iterator auto it = vec.begin();
+    ++(++it);
+    --it;
+    it = it + 2;
+
+    std::ranges::sort(vec, std::greater{});
+    print(vec, "vec");
+
+    auto pos = Explain::ver_4::find_if(vec.begin(), vec.end(), is_even);
+    CHECK(pos != vec.end());
+    CHECK(*pos == 8);
+}
+
+///////////////////////////////////////////////////////////////////
+// concepts
+
+template <typename T>
+concept Addable = requires(T a, T b) {
+    a + b;
+};
+
+static_assert(Addable<int>);
+static_assert(Addable<std::string>);
+static_assert(!Addable<std::vector<int>>);
+static_assert(std::integral<int>);
+static_assert(!std::integral<double>);
+
+namespace Unconstrained
+{
+    template <typename T>
+    auto add(T a, T b)
+    {
+        return a + b;
+    }
+} // namespace Unconstrained
+
+namespace Constrained
+{
+    inline namespace ver_1
+    {
+        template <typename T>
+            requires Addable<T>
+        auto add(T a, T b)
+        {
+            return a + b;
+        }
+    } // namespace ver_1
+
+    namespace ver_2
+    {
+        template <Addable T>
+        auto add(T a, T b)
+        {
+            return a + b;
+        }
+    } // namespace ver_2
+} // namespace Constrained
+
+TEST_CASE("using concepts")
+{
+    auto str = Unconstrained::add("abc"s, "def"s);
+    CHECK(str == "abcdef");
+
+    std::integral auto vec = Constrained::add(5, 665); // std::integral<decltype(vec)>
+}
+
+//////////////////////////////////////////////////////////////////////
+
+void do_work(const std::string& desc)
+{
+	std::cout << "work: " << desc << "\n";
+}
+
+struct DoWork
+{
+	int counter{};
+
+	void operator()(const std::string& desc)
+	{
+		++counter;
+		std::cout << std::format("WORK: {}; counter: {};\n", desc, counter);
+	}
+};
+
+TEST_CASE("callback")
+{
+	SECTION("C-style")
+	{
+		void (*callback)(const std::string&) = nullptr;
+
+		callback = do_work;
+
+		callback("START");
+		callback("STOP");
+
+		callback = [](const auto& desc) { std::cout << "Lambda: " << desc << "\n"; };
+		callback("HELLO");
+	}
+
+	SECTION("modern C++")
+	{
+		std::function<void(const std::string&)> callback;
+		CHECK_THROWS_AS(callback("abc"), std::bad_function_call);
+
+		callback = do_work;
+		callback("START");
+
+		callback = DoWork();
+		callback("A");
+		callback("B");
+		callback("C");
+
+		uint64_t counter{};
+		callback = [&counter](const auto& desc) { std::cout << std::format("Lambda: {}\n", desc); ++counter; };
+		callback("D");
+		callback("E");
+		callback("F");
+		CHECK(counter == 3);
+	}
+}
+
+struct Robot
+{
+	int id;
+	std::function<void()> on_start;
+	std::function<void(std::span<std::byte>)> on_data_ready;
+
+	// Robot(int id, std::function<void(std::span<std::byte>)> data_ready_callback) : id{id}, on_data_ready{data_ready_callback}
+	// {
+	// 	bootstrap();
+	// }
+
+	template <std::invocable<std::span<std::byte>> Callback>
+	Robot(int id, Callback data_ready_callback) : id{id}, on_data_ready{data_ready_callback}
+	{
+		bootstrap();
+	}
+
+	void bootstrap()
+	{
+		on_start = [this](){ std::cout << std::format("Robot#{} has started...", id); };
+	}
+
+	void run()
+	{
+		on_start();
+
+		std::vector<std::byte> data{std::byte{42}, std::byte{45}, std::byte{44}};
+		
+		if (on_data_ready)
+			on_data_ready(data);
+	}
+};
+
+TEST_CASE("using callbacks")
+{
+	std::vector<int> data;
+
+	auto on_data_ready = [&data](std::span<std::byte> received) { 
+		for(std::byte b : received)
+		{
+			data.push_back(std::to_integer<int>(b));
+		}
+	};
+
+	Robot robot(665, on_data_ready);
+	robot.run();
+
+	CHECK(data == std::vector{42, 45, 44});
 }
