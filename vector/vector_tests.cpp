d8cacd76145f3f0d93aeffc6d0057fd5edbde20d
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-03-20 13:33:44 +0100

    Vector - 1

--- vector/vector_tests.cpp
+++ vector/vector_tests.cpp
@@ -1,28 +1,60 @@
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <ranges>
 #include <string>
 #include <vector>
-#include <ranges>
-#include <algorithm>
 
 using namespace std::literals;
 
 namespace ModernCpp
 {
-	class Vector
-	{};
-}
+    class Vector
+    {
+    public:
+		using iterator = int*;
+
+        Vector(size_t size)
+            : size_{size}
+        {
+			items_ = new int[size_]{};
+        }
+
+        size_t size()
+        {
+            return size_;
+        }
+
+		iterator begin()
+		{
+			return items_;
+		}
+
+		iterator end()
+		{
+			return items_ + size_;
+		}
+
+    private:
+        size_t size_;
+		int* items_;
+    };
+} // namespace ModernCpp
 
 namespace rng = std::ranges;
 
 TEST_CASE("Vector")
-{	
-	using ModernCpp::Vector;
+{
+    using ModernCpp::Vector;
 
-	SECTION("constructed with size")
-	{
-		Vector vec(10);
-		CHECK(vec.size() == 10)
-		CHECK(rng::all_of(vec, [](int x) { return x == 0; }));
-	}
+    SECTION("constructed with size")
+    {
+        Vector vec(10);
+        CHECK(vec.size() == 10);
+
+		SECTION("all items set to zero")
+		{
+			CHECK(rng::all_of(vec, [](int x) { return x == 0; }));
+		}
+    }
 }
