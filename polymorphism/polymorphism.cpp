57f2a85a0c8cae6895f4e8a429bb3cbb8d8c67e1
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-03-22 11:47:37 +0100

    Polymorphism - 1

--- polymorphism/polymorphism.cpp
+++ polymorphism/polymorphism.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 
 using namespace std::literals;
 
@@ -34,4 +35,36 @@ TEST_CASE("polymorphism")
         rect.move(44, 55);
         rect.draw();
     }
+
+    SECTION("pointers & reference to base class")
+    {
+        Shape* ptr_shape{};
+
+        Circle crc{10, 20, 50};
+        Rectangle rect{77, 99, 100, 200};
+
+        ptr_shape = &crc;
+        ptr_shape->draw();
+
+        ptr_shape = &rect;
+        ptr_shape->draw();
+
+        Shape& ref_shape = rect;
+        ref_shape.draw();
+    }
+}
+
+TEST_CASE("polymorphic objects")
+{
+    using namespace Drawing;
+
+    std::vector<std::unique_ptr<Shape>> shapes;
+
+    shapes.push_back(std::make_unique<Rectangle>(10, 20, 60, 55));
+    shapes.push_back(std::make_unique<Circle>(17, 87, 60));
+    shapes.push_back(std::make_unique<Rectangle>(7, 78, 66, 33));
+    shapes.push_back(std::make_unique<Circle>(78, 80, 30));
+
+    for(const auto& shp : shapes)
+        shp->draw();   
 }
--- polymorphism/shape.hpp
+++ polymorphism/shape.hpp
@@ -26,7 +26,13 @@ namespace Drawing
             coord_.translate(dx, dy);
         }
 
-        void draw() const;
+        virtual void draw() const;
+
+        virtual ~Shape() 
+        {
+            std::cout << "~Shape()\n";
+        }
+
     protected:
         Point coord() const
         {
@@ -37,14 +43,47 @@ namespace Drawing
     class Circle : public Shape
     {
         uint16_t radius_;
-    public:
-        Circle(int x = 0, int y = 0, uint16_t r = 0) : Shape{x, y}, radius_{r}
-        {}
 
-        void draw() const
+    public:
+        Circle(int x = 0, int y = 0, uint16_t r = 0)
+            : Shape{x, y}
+            , radius_{r}
+        { }
+
+        void draw() const override
         {
             std::cout << "Drawing Circle at " << coord() << " with radius " << radius_ << "\n";
         }
+
+        ~Circle() override
+        {
+            std::cout << "~Circle()\n";
+        }
+    };
+
+    class Rectangle : public Shape
+    {
+    public:
+        Rectangle(int x = 0, int y = 0, uint16_t w = 0, uint16_t h = 0)
+            : Shape{x, y}
+            , w_{w}
+            , h_{h}
+        {
+        }
+
+        void draw() const override
+        {
+            std::cout << "Drawing Rectangle at " << coord() << " with dimensions (width: " << w_ << ", height: " << h_ << ")\n";
+        }
+
+        ~Rectangle() override
+        {
+            std::cout << "~Rectangle()\n";
+        }
+
+    private:
+        uint16_t w_;
+        uint16_t h_;
     };
 } // namespace Drawing
 
